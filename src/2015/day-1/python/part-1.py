f028f35f1893d6ad4916b9b8095719b0ce997562
jasonlotz <jasonlotz@gmail.com>
2023-11-29 10:34:50 -0500

    Completed 2015 day 1 part 1.

---
+++ src/2015/day-1/python/part-1.py
@@ -0,0 +1,12 @@
+# Run: python src/2015/day-1/python/part-1.py
+
+import re
+
+input_string = 'src/2015/day-1/input.txt'
+
+with open(input_string, 'r') as input_file:
+    for line in input_file:
+        left = len(line.split('('))
+        right = len(line.split(')'))
+
+        print("Floor #: " + str(left - right))
