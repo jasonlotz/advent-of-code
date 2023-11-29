eb13a250eeed6677fef12fd35eac890404275a92
jasonlotz <jasonlotz@gmail.com>
2023-11-29 12:18:07 -0500

    Completed 2015 day 1 part 2.

---
+++ src/2015/day-1/ts/day-1.ts
@@ -0,0 +1,28 @@
+// Run: ts-node src/2015/day-1/ts/day-1.ts
+
+import * as fs from "fs";
+
+const input = fs.readFileSync("src/2015/day-1/input.txt", "utf-8");
+
+// Part 1
+const left = input.split("(").length - 1;
+const right = input.split(")").length - 1;
+
+console.log(`Floor #: ${left - right}`);
+
+// Part 2
+const chars = input.split("");
+let floor = 0;
+
+for (let i = 0; i < chars.length; i++) {
+    if (chars[i] === "(") {
+        floor++;
+    } else if (chars[i] === ")") {
+        floor--;
+    }
+
+    if (floor === -1) {
+        console.log(`Basement at position #: ${i + 1}`);
+        break;
+    }
+}
