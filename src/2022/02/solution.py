d004baff502dec6cb5355e9e91bc600d5ab24298
jasonlotz <jasonlotz@gmail.com>
2023-11-30 22:39:32 -0500

    Completed day 2 part 2.

--- src/2022/02/solution.py
+++ src/2022/02/solution.py
@@ -23,6 +23,24 @@ outcome_points_map = {
     ('Z', 'A'): LOSE_POINTS,
 }
 
+throw_win_map = {
+    'C': 'X',
+    'A': 'Y',
+    'B': 'Z'
+}
+
+throw_tie_map = {
+    'A': 'X',
+    'B': 'Y',
+    'C': 'Z',
+}
+
+throw_lose_map = {
+    'B': 'X',
+    'C': 'Y',
+    'A': 'Z'
+}
+
 
 def round(theirs, ours):
     points = shape_points_map[ours]
@@ -31,13 +49,35 @@ def round(theirs, ours):
     return points
 
 
+def throw_round(theirs, ours):
+    new_ours = ours
+
+    if ours == "X":
+        # Lose
+        new_ours = throw_lose_map[theirs]
+    elif ours == "Y":
+        # Tie
+        new_ours = throw_tie_map[theirs]
+    elif ours == "Z":
+        # Win
+        new_ours = throw_win_map[theirs]
+
+    points = shape_points_map[new_ours]
+    points += outcome_points_map[new_ours, theirs]
+
+    return points
+
+
 input_string = 'src/2022/02/input.txt'
 
 total_points = 0
+throw_total_points = 0
 
 with open(input_string, 'r') as input_file:
     for line in input_file:
         theirs, ours = line.replace("\n", "").split(" ")
         total_points += round(theirs, ours)
+        throw_total_points += throw_round(theirs, ours)
 
 print(f"Total points: {total_points}")
+print(f"Throw total points: {throw_total_points}")
