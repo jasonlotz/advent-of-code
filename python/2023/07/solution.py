5208aa5155f78f8337171d83b03267fd468fdf1c
jasonlotz <jasonlotz@gmail.com>
2023-12-07 13:10:00 -0500

    Completed 2023 day 7.

--- python/2023/07/solution.py
+++ python/2023/07/solution.py
@@ -30,42 +30,93 @@ CARD_VALUES = {
 }
 
 
+def calc_hand_counter_value(hand_counter):
+    hand_value = HAND_TYPES[tuple(sorted(hand_counter.values(), reverse=True))]
+    return hand_value
+
+
 def hand_value(hand: str) -> int:
     hand_counter = Counter(hand)
-    hand_tuple = tuple(sorted(hand_counter.values(), reverse=True))
+    hand_value = calc_hand_counter_value(hand_counter)
 
-    # Calculate hand value
-    hand_value = HAND_TYPES[hand_tuple]
+    high_card_score = calc_high_card_score(hand)
 
-    # Calculate high card score
+    return hand_value, high_card_score
+
+
+def calc_high_card_score(hand, jokers=False):
+    card_values = CARD_VALUES.copy()
+    if jokers:
+        card_values["J"] = 1
+
     high_card_score = 0
     multiplier = 10000000000
     for card in hand:
-        high_card_score += CARD_VALUES[card] * multiplier
+        high_card_score += card_values[card] * multiplier
         multiplier /= 100
+    return high_card_score
 
-    return hand_value, high_card_score
+
+def hand_value_with_jokers(hand: str) -> int:
+    hand_counter = Counter(hand)
+    hand_value = calc_hand_counter_value(hand_counter)
+    high_card_score = calc_high_card_score(hand, True)
+    new_hand = hand
+
+    possible_hands = []
+    if "J" in hand_counter.keys():
+        print(f"Joker found in hand: {hand}")
+
+        # Loop over all possible hands for the joker and add to possible hands
+        for card in CARD_VALUES.keys():
+            possible_hand = hand.replace("J", card)
+            possible_hand_counter = Counter(possible_hand)
+            possible_hand_value = calc_hand_counter_value(
+                possible_hand_counter)
+
+            possible_hands.append((possible_hand, possible_hand_value))
+
+        # Sort the possible hands by hand value
+        possible_hands.sort(key=lambda x: (x[1]), reverse=True)
+        new_hand = possible_hands[0][0]
+        hand_value = possible_hands[0][1]
+
+    return new_hand, hand_value, high_card_score
 
 
 def main():
     inputs = open(INPUT_PATH).read().splitlines()
 
     hands = []
+    hands_with_jokers = []
     total_winnings = 0
+    total_winnings_with_jokers = 0
 
     for inputs in inputs:
         hand, bid = inputs.split()
 
         value, high_card_score = hand_value(hand)
-
         hands.append((hand, value, high_card_score, int(bid)))
         hands.sort(key=lambda x: (x[1], x[2]))
 
+        new_hand, value_with_jokers, high_card_score_with_jokers = hand_value_with_jokers(
+            hand)
+        hands_with_jokers.append(
+            (hand, new_hand, value_with_jokers, high_card_score_with_jokers, int(bid)))
+        hands_with_jokers.sort(key=lambda x: (x[2], x[3]))
+
+    print("Scored Hand (No Jokers)")
     for i, hand in enumerate(hands, 1):
-        print(i, hand, hand[3] * i)
+        # print(i, hand, hand[3] * i)
         total_winnings += hand[3] * i
 
-    print(total_winnings)
+    print("Scored Hand (Jokers)")
+    for i, hand in enumerate(hands_with_jokers, 1):
+        print(i, hand, hand[4] * i)
+        total_winnings_with_jokers += hand[4] * i
+
+    print(f"Total winnings: {total_winnings}")
+    print(f"Total winnings with jokers: {total_winnings_with_jokers}")
 
 
 if __name__ == "__main__":
