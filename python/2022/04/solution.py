646b033e80096990dd6e7f73ba8e3ed9d503e391
jasonlotz <jasonlotz@gmail.com>
2023-12-07 08:58:14 -0500

    Algorithm refactoring.

--- python/2022/04/solution.py
+++ python/2022/04/solution.py
@@ -13,10 +13,7 @@ for assignment in assignments:
             (pair2[0] >= pair1[0] and pair2[1] <= pair1[1])):
         fully_contained += 1
 
-    if ((pair1[0] <= pair2[0] and pair1[1] >= pair2[0]) or
-            (pair1[0] <= pair2[1] and pair1[1] >= pair2[1]) or
-            (pair2[0] <= pair1[0] and pair2[1] >= pair1[0]) or
-            (pair2[0] <= pair1[0] and pair2[1] >= pair1[1])):
+    if (pair1[1] >= pair2[0] and pair2[1] >= pair1[0]):
         partially_contained += 1
 
 
