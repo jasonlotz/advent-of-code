5cef6430c5443d5668773f0ed472ecc874bbd678
jasonlotz <jasonlotz@gmail.com>
2023-12-10 10:48:55 -0500

    Replaced accidentally deleted file.

--- python/2023/10/solution.py
+++ python/2023/10/solution.py
@@ -1,117 +1,107 @@
-from pprint import pprint
-
-INPUT_PATH = "input-files/2023/10/input-sample.txt"
-
-
-N_S = "|"  # is a vertical pipe connecting north and south
-E_W = "-"  # is a horizontal pipe connecting east and west
-N_E = "L"  # is a 90-degree bend connecting north and east
-N_W = "J"  # is a 90-degree bend connecting north and west
-S_W = "7"  # is a 90-degree bend connecting south and west
-S_E = "F"  # is a 90-degree bend connecting south and east
-NO_PIPE = "."  # is ground; there is no pipe in this tile
-ANIMAL = "S"  # is the starting position of the animal
-
-
-def find_animal(maze):
-    for i in range(len(maze)):
-        for j in range(len(maze[i])):
-            if maze[i][j] == ANIMAL:
-                print(f"Animal: {i}, {j}")
-                return (i, j)
-
-
-def find_start_end(maze, animal):
-    start_end = []
-
-    # North
-    north_coord = (animal[0]-1, animal[1])
-    north_value = maze[north_coord[0]][north_coord[1]]
-
-    print(f"North: {north_coord} -> {north_value}")
-    if north_value == N_S:
-        start_end.append(north_coord)
-
-    # South
-    south_coord = (animal[0]+1, animal[1])
-    south_value = maze[south_coord[0]][south_coord[1]]
-
-    print(f"South: {south_coord} -> {south_value}")
-    if south_value == N_S:
-        start_end.append(south_coord)
-
-    # East
-    east_coord = (animal[0], animal[1]+1)
-    east_value = maze[east_coord[0]][east_coord[1]]
-
-    print(f"East: {east_coord} -> {east_value}")
-    if east_value in (E_W, N_W, S_W):
-        start_end.append(east_coord)
-
-    # West
-    west_coord = (animal[0], animal[1]-1)
-    west_value = maze[west_coord[0]][west_coord[1]]
-
-    print(f"West: {west_coord} -> {west_value}")
-    if west_value in (E_W, N_E, S_E):
-        start_end.append(west_coord)
-
-    print(f"Start & end: {start_end}")
-
-    if len(start_end) != 2:
-        raise Exception("Invalid maze")
-
-    return start_end[0], start_end[1]
-
-
-def follow_pipe(maze, animal):
-    start, end = find_start_end(maze, animal)
-
-    current = start
-    move_count = 0
-
-    while current != end:
-        current_value = maze[current[0]][current[1]]
-
-        if current_value == N_S:
-            current = (current[0]-1, current[1])
-        elif current_value == E_W:
-            current = (current[0], current[1]+1)
-        elif current_value == N_E:
-            current = (current[0]-1, current[1])
-        elif current_value == N_W:
-            current = (current[0]-1, current[1])
-        elif current_value == S_W:
-            current = (current[0]+1, current[1])
-        elif current_value == S_E:
-            current = (current[0]+1, current[1])
-        else:
-            raise Exception("Invalid maze")
-
-        move_count += 1
-        print(f"Move: {move_count}")
-        print(f"Current: {current}")
+INPUT_PATH = "input-files/2023/10/input.txt"
 
 
 def parse_input():
-    maze = []
     with open(INPUT_PATH, "r") as input_file:
-        lines = input_file.readlines()
+        input = input_file.read().strip().splitlines()
 
-        for line in lines:
-            maze.append(list(line.strip()))
-
-    return maze
+    return input
 
 
-def part_1():
-    maze = parse_input()
-    animal = find_animal(maze)
-    follow_pipe(maze, animal)
+class PipeLoop:
+    def __init__(self, input):
+        self.map = input
+        self.directions = {
+            "left": (0, -1), "right": (0, 1), "up": (-1, 0), "down": (1, 0)}
+        self.pipe_directions = {
+            ("L", "left"): "up",
+            ("F", "up"): "right",
+            ("7", "right"): "down",
+            ("J", "down"): "left",
+            ("F", "left"): "down",
+            ("7", "up"): "left",
+            ("J", "right"): "up",
+            ("L", "down"): "right",
+            ("-", "left"): "left",
+            ("-", "right"): "right",
+            ("|", "up"): "up",
+            ("|", "down"): "down",
+        }
+        self.start = self.find_start_point()
+        self.loop = self.follow_pipe()
+        self.enclosed = self.find_enclosed()
+
+    def find_start_point(self):
+        for i in range(len(self.map)):
+            for j in range(len(self.map[0])):
+                if self.map[i][j] == "S":
+                    return i, j
+
+    def traverse(self, current_pipe, current_direction):
+        (r, c) = current_pipe
+
+        next_direction = self.pipe_directions[(
+            self.map[r][c], current_direction)]
+
+        next_pipe = tuple(
+            map(sum, zip(current_pipe, self.directions[next_direction])))
+
+        return next_pipe, next_direction
+
+    def from_start_point(self):
+        for direction in self.directions:
+            (dr, dc) = tuple(
+                map(sum, zip(self.start, self.directions[direction])))
+
+            if (self.map[dr][dc], direction) in self.pipe_directions:
+                return (dr, dc), direction
+
+    def follow_pipe(self):
+        current, direction = self.from_start_point()
+        (r, c) = current
+        loop = {current: (self.map[r][c], direction)}
+
+        while current != self.start:
+            current, direction = self.traverse(current, direction)
+            (r, c) = current
+            loop[current] = self.map[r][c], direction
+        # Replace "S"
+        else:
+            for pipe in ("L", "F", "7", "J", "|", "-"):
+                if (pipe, direction) in self.pipe_directions:
+                    next_direction = self.pipe_directions[(pipe, direction)]
+                    next_pipe = tuple(
+                        map(sum, zip(current, self.directions[next_direction])))
+                    if next_pipe == list(loop)[0]:
+                        loop[current] = (pipe, direction)
+                        return loop
+
+    def find_enclosed(self):
+        enclosed = set()
+        inside_loop = {}
+        loop_direction = 0
+
+        # Get loop"s direction
+        for r in range(len(self.map)):
+            for c in range(len(self.map[0])):
+                if (r, c) in self.loop:
+                    if self.loop[(r, c)][1] == "up" or self.pipe_directions[self.loop[(r, c)]] == "up":
+                        loop_direction = "up"
+                    elif self.loop[(r, c)][1] == "down" or self.pipe_directions[self.loop[(r, c)]] == "down":
+                        loop_direction = "down"
+                    # Get most outside loop's direction to check
+                    if inside_loop == {}:
+                        inside_loop = {loop_direction: True}
+                elif loop_direction in inside_loop:
+                    enclosed.add((r, c))
+        return enclosed
 
 
 def main():
-    part_1()
+    input = parse_input()
+    pipes = PipeLoop(input)
+    print("Part 1:", len(pipes.loop) // 2)
+    print("Part 2:", len(pipes.enclosed))
 
 
 if __name__ == "__main__":
