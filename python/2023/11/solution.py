61fd4292d5ed22d264951f65dd138f2676d298d4
jasonlotz <jasonlotz@gmail.com>
2023-12-11 20:18:15 -0500

    Completed 2023 day 11.

---
+++ python/2023/11/solution.py
@@ -0,0 +1,51 @@
+INPUT_PATH = "input-files/2023/11/input-sample.txt"
+
+
+def parse_input():
+    return open(INPUT_PATH).read().splitlines()
+
+
+# Find Manhattan distance between every pair of galaxies
+def manhattan_distance_total(grid, expand_scale=0):
+    empty_rows = [r for r, row in enumerate(grid) if all(ch == "." for ch in row)]
+    empty_cols = [c for c, col in enumerate(zip(*grid)) if all(ch == "." for ch in col)]
+
+    galaxies = [(r, c) for r, row in enumerate(grid) for c, ch in enumerate(row) if ch == "#"]
+
+    print(f"Galaxies: {galaxies}")
+
+    total = 0
+
+    for i, (r1, c1) in enumerate(galaxies):
+        print(f"r1: {r1}, c1: {c1}")
+        for (r2, c2) in galaxies[:i]:
+            print(f"r2: {r2}, c2: {c2}")
+            for r in range(min(r1, r2), max(r1, r2)):
+                total += expand_scale if r in empty_rows else 1
+                print(f"Current total (r loop): {total}")
+            for c in range(min(c1, c2), max(c1, c2)):
+                total += expand_scale if c in empty_cols else 1
+                print(f"Current total (c loop): {total}")
+
+    return total
+
+
+def part1():
+    grid = parse_input()
+    total = manhattan_distance_total(grid, 2)
+    print(f"Total (part 1): {total}")
+
+
+def part2():
+    grid = parse_input()
+    total = manhattan_distance_total(grid, 1000000)
+    print(f"Total (part 2): {total}")
+
+
+def main():
+    part1()
+    part2()
+
+
+if __name__ == "__main__":
+    main()
---
+++ python/solution-template.py
@@ -0,0 +1,23 @@
+INPUT_PATH = ""
+
+
+def parse_input():
+    pass
+
+
+def part1():
+    parse_input()
+    pass
+
+
+def part2():
+    pass
+
+
+def main():
+    part1()
+    part2()
+
+
+if __name__ == "__main__":
+    main()
