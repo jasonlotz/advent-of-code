d66ebbdfc7962056d5d6e8211b3732ead106ad1f
jasonlotz <jasonlotz@gmail.com>
2024-02-24 08:26:54 -0500

    Name things better

--- go/2017/02/solution_test.go
+++ go/2017/02/solution_test.go
@@ -8,28 +8,29 @@ func TestFindMinMax(t *testing.T) {
 	input := []int{5, 1, 9, 5}
 	expectedMin := 1
 	expectedMax := 9
-	min, max := findMinMax(input)
-	testMinMaxResult(input, expectedMin, expectedMax, min, max, t)
+	resultMin, resultMax := findMinMax(input)
+	testMinMaxResult(input, expectedMin, expectedMax, resultMin, resultMax, t)
 
 	input = []int{7, 5, 3}
 	expectedMin = 3
 	expectedMax = 7
-	min, max = findMinMax(input)
-	testMinMaxResult(input, expectedMin, expectedMax, min, max, t)
+	resultMin, resultMax = findMinMax(input)
+	testMinMaxResult(input, expectedMin, expectedMax, resultMin, resultMax, t)
 
 	input = []int{2, 4, 6, 8}
 	expectedMin = 2
 	expectedMax = 8
-	min, max = findMinMax(input)
-	testMinMaxResult(input, expectedMin, expectedMax, min, max, t)
+	resultMin, resultMax = findMinMax(input)
+
+	testMinMaxResult(input, expectedMin, expectedMax, resultMin, resultMax, t)
 }
 
-func testMinMaxResult(input []int, expectedMin int, expectedMax int, min int, max int, t *testing.T) {
-	if expectedMin != min {
-		t.Error(input, "-> expected min of", expectedMin, "but got", min)
+func testMinMaxResult(input []int, expectedMin int, expectedMax int, resultMin int, resultMax int, t *testing.T) {
+	if expectedMin != resultMin {
+		t.Error(input, "-> expected min of", expectedMin, "but got", resultMin)
 	}
-	if expectedMax != max {
-		t.Error(input, "-> expected max of", expectedMax, "but got", max)
+	if expectedMax != resultMax {
+		t.Error(input, "-> expected max of", expectedMax, "but got", resultMax)
 	}
 }
 
@@ -37,28 +38,27 @@ func TestFindDivisible(t *testing.T) {
 	input := []int{5, 9, 2, 8}
 	expectedI := 8
 	expectedJ := 2
-	i, j := findDivisible(input)
-	testDivisibleResult(input, expectedI, expectedJ, i, j, t)
+	resultI, resultJ := findDivisible(input)
+	testDivisibleResult(input, expectedI, expectedJ, resultI, resultJ, t)
 
 	input = []int{9, 4, 7, 3}
 	expectedI = 9
 	expectedJ = 3
-	i, j = findDivisible(input)
-	testDivisibleResult(input, expectedI, expectedJ, i, j, t)
+	resultI, resultJ = findDivisible(input)
+	testDivisibleResult(input, expectedI, expectedJ, resultI, resultJ, t)
 
 	input = []int{3, 8, 6, 5}
 	expectedI = 6
 	expectedJ = 3
-	i, j = findDivisible(input)
-	testDivisibleResult(input, expectedI, expectedJ, i, j, t)
-
+	resultI, resultJ = findDivisible(input)
+	testDivisibleResult(input, expectedI, expectedJ, resultI, resultJ, t)
 }
 
-func testDivisibleResult(input []int, expectedI int, expectedJ int, i int, j int, t *testing.T) {
-	if expectedI != i {
-		t.Error(input, "-> expected i of", expectedI, "but got", i)
+func testDivisibleResult(input []int, expectedI int, expectedJ int, resultI int, resultJ int, t *testing.T) {
+	if expectedI != resultI {
+		t.Error(input, "-> expected i of", expectedI, "but got", resultI)
 	}
-	if expectedJ != j {
-		t.Error(input, "-> expected j of", expectedJ, "but got", j)
+	if expectedJ != resultJ {
+		t.Error(input, "-> expected j of", expectedJ, "but got", resultJ)
 	}
 }
