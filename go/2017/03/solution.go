22818eb1a9aa7156f32237c5eb3334c2f0fbb4f5
jasonlotz <jasonlotz@gmail.com>
2024-02-24 09:56:22 -0500

    Complete 2017 day 3 in Go

---
+++ go/2017/03/solution.go
@@ -0,0 +1,87 @@
+package main
+
+import (
+	"fmt"
+)
+
+type DirectionType string
+
+var Direction = struct {
+	RIGHT DirectionType
+	UP    DirectionType
+	LEFT  DirectionType
+	DOWN  DirectionType
+}{
+	RIGHT: "right",
+	UP:    "up",
+	LEFT:  "left",
+	DOWN:  "down",
+}
+
+func main() {
+	input := 23
+	x, y := getCoordForSpiralGrid(input)
+	distance := abs(x) + abs(y)
+	fmt.Printf("%d  -> %d, %d (%d)\n", input, x, y, distance)
+}
+
+func abs(int int) int {
+	if int < 0 {
+		return -int
+	}
+	return int
+}
+
+func getCoordForSpiralGrid(input int) (int, int) {
+	x, y := 0, 0
+	rightBound := 1
+	upBound := 1
+	leftBound := -1
+	downBound := -1
+	value := 1
+	direction := Direction.RIGHT
+
+	for value < input {
+		if direction == Direction.RIGHT {
+			if x < rightBound {
+				x++
+				value++
+			} else {
+				direction = Direction.UP
+				rightBound++
+			}
+		}
+
+		if direction == Direction.UP {
+			if y < upBound {
+				y++
+				value++
+			} else {
+				direction = Direction.LEFT
+				upBound++
+			}
+		}
+
+		if direction == Direction.LEFT {
+			if x > leftBound {
+				x--
+				value++
+			} else {
+				direction = Direction.DOWN
+				leftBound--
+			}
+		}
+
+		if direction == Direction.DOWN {
+			if y > downBound {
+				y--
+				value++
+			} else {
+				direction = Direction.RIGHT
+				downBound--
+			}
+		}
+	}
+
+	return x, y
+}
