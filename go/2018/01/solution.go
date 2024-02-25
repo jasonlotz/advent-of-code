4eeb98db851c47b07220f99d5eb51a11fff69f84
jasonlotz <jasonlotz@gmail.com>
2024-02-25 09:55:51 -0500

    feat: complete 2018 day 1 in Go

---
+++ go/2018/01/solution.go
@@ -0,0 +1,75 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+var INPUT_FILE = "../../../input-files/2018/01/input.txt"
+
+func main() {
+	part1()
+	part2()
+}
+
+func part1() {
+	inputs := processFile(INPUT_FILE)
+	sum := sum(inputs)
+	fmt.Println("Part 1:", sum)
+}
+
+func part2() {
+	inputs := processFile(INPUT_FILE)
+	result := seenTwice(inputs)
+	fmt.Println("Part 2:", result)
+}
+
+func sum(inputs []int) int {
+	sum := 0
+	for _, i := range inputs {
+		sum += i
+	}
+
+	return sum
+}
+
+func seenTwice(inputs []int) int {
+	seen := make(map[int]bool)
+	sum := 0
+	i := 0
+
+	for {
+		seen[sum] = true
+		newSum := sum + inputs[i]
+		if seen[newSum] {
+			return newSum
+		}
+
+		sum = newSum
+		i = (i + 1) % len(inputs)
+	}
+}
+
+func processFile(inputFile string) []int {
+	file, err := os.Open(inputFile)
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	processedFile := make([]int, 0)
+
+	for scanner.Scan() {
+		intVal, err := strconv.Atoi(scanner.Text())
+		if err != nil {
+			panic(err)
+		}
+
+		processedFile = append(processedFile, intVal)
+	}
+
+	return processedFile
+}
---
+++ go/2018/01/solution_test.go
@@ -0,0 +1,40 @@
+package main
+
+import "testing"
+
+func TestSum(t *testing.T) {
+	tests := []struct {
+		input    []int
+		expected int
+	}{
+		{[]int{1, 1, 1}, 3},
+		{[]int{1, 1, -2}, 0},
+		{[]int{-1, -2, -3}, -6},
+	}
+
+	for _, test := range tests {
+		result := sum(test.input)
+		if result != test.expected {
+			t.Errorf("%v -> expected %d for part 1 but got %d", test.input, test.expected, result)
+		}
+	}
+}
+
+func TestSeenTwice(t *testing.T) {
+	tests := []struct {
+		input    []int
+		expected int
+	}{
+		{[]int{1, -1}, 0},
+		{[]int{3, 3, 4, -2, -4}, 10},
+		{[]int{-6, 3, 8, 5, -6}, 5},
+		{[]int{7, 7, -2, -7, -4}, 14},
+	}
+
+	for _, test := range tests {
+		result := seenTwice(test.input)
+		if result != test.expected {
+			t.Errorf("%v -> expected %d for part 2 but got %d", test.input, test.expected, result)
+		}
+	}
+}
