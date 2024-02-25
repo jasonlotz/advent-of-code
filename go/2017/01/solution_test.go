c31df05415e5fd08a9dbdb78ace2e42133a07105
jasonlotz <jasonlotz@gmail.com>
2024-02-25 10:11:18 -0500

    test: updated tests for 2017 day 1 & 2 in Go

--- go/2017/01/solution_test.go
+++ go/2017/01/solution_test.go
@@ -5,56 +5,40 @@ import (
 )
 
 func TestSumCaptchaNext(t *testing.T) {
-	input := "1122"
-	expected := 3
-	result := sumCaptchaNext(input)
-	testResult(input, result, expected, t)
+	tests := []struct {
+		input    string
+		expected int
+	}{
+		{"1122", 3},
+		{"1111", 4},
+		{"1234", 0},
+		{"91212129", 9},
+	}
 
-	input = "1111"
-	expected = 4
-	result = sumCaptchaNext(input)
-	testResult(input, result, expected, t)
-
-	input = "1234"
-	expected = 0
-	result = sumCaptchaNext(input)
-	testResult(input, result, expected, t)
-
-	input = "91212129"
-	expected = 9
-	result = sumCaptchaNext(input)
-	testResult(input, result, expected, t)
+	for _, test := range tests {
+		result := sumCaptchaNext(test.input)
+		if result != test.expected {
+			t.Errorf("%s -> expected %d for sumCaptchaNext but got %d", test.input, test.expected, result)
+		}
+	}
 }
 
 func TestSumCaptchaHalfway(t *testing.T) {
-	input := "1212"
-	expected := 6
-	result := sumCaptchaHalfway(input)
-	testResult(input, result, expected, t)
+	tests := []struct {
+		input    string
+		expected int
+	}{
+		{"1212", 6},
+		{"1221", 0},
+		{"123425", 4},
+		{"123123", 12},
+		{"12131415", 4},
+	}
 
-	input = "1221"
-	expected = 0
-	result = sumCaptchaHalfway(input)
-	testResult(input, result, expected, t)
-
-	input = "123425"
-	expected = 4
-	result = sumCaptchaHalfway(input)
-	testResult(input, result, expected, t)
-
-	input = "123123"
-	expected = 12
-	result = sumCaptchaHalfway(input)
-	testResult(input, result, expected, t)
-
-	input = "12131415"
-	expected = 4
-	result = sumCaptchaHalfway(input)
-	testResult(input, result, expected, t)
-}
-
-func testResult(input string, result int, expected int, t *testing.T) {
-	if result != expected {
-		t.Error(input, "-> expected result of", expected, "but got", result)
+	for _, test := range tests {
+		result := sumCaptchaHalfway(test.input)
+		if result != test.expected {
+			t.Errorf("%s -> expected %d for sumCaptchaHalfway but got %d", test.input, test.expected, result)
+		}
 	}
 }
--- go/2017/02/solution_test.go
+++ go/2017/02/solution_test.go
@@ -5,60 +5,47 @@ import (
 )
 
 func TestFindMinMax(t *testing.T) {
-	input := []int{5, 1, 9, 5}
-	expectedMin := 1
-	expectedMax := 9
-	resultMin, resultMax := findMinMax(input)
-	testMinMaxResult(input, expectedMin, expectedMax, resultMin, resultMax, t)
-
-	input = []int{7, 5, 3}
-	expectedMin = 3
-	expectedMax = 7
-	resultMin, resultMax = findMinMax(input)
-	testMinMaxResult(input, expectedMin, expectedMax, resultMin, resultMax, t)
-
-	input = []int{2, 4, 6, 8}
-	expectedMin = 2
-	expectedMax = 8
-	resultMin, resultMax = findMinMax(input)
-
-	testMinMaxResult(input, expectedMin, expectedMax, resultMin, resultMax, t)
-}
-
-func testMinMaxResult(input []int, expectedMin int, expectedMax int, resultMin int, resultMax int, t *testing.T) {
-	if expectedMin != resultMin {
-		t.Error(input, "-> expected min of", expectedMin, "but got", resultMin)
+	test := []struct {
+		input       []int
+		expectedMin int
+		expectedMax int
+	}{
+		{[]int{5, 1, 9, 5}, 1, 9},
+		{[]int{7, 5, 3}, 3, 7},
+		{[]int{2, 4, 6, 8}, 2, 8},
 	}
-	if expectedMax != resultMax {
-		t.Error(input, "-> expected max of", expectedMax, "but got", resultMax)
+
+	for _, test := range test {
+		resultMin, resultMax := findMinMax(test.input)
+
+		if test.expectedMin != resultMin {
+			t.Error(test.input, "-> expected min of", test.expectedMin, "but got", resultMin)
+		}
+		if test.expectedMax != resultMax {
+			t.Error(test.input, "-> expected max of", test.expectedMax, "but got", resultMax)
+		}
 	}
 }
 
 func TestFindDivisible(t *testing.T) {
-	input := []int{5, 9, 2, 8}
-	expectedI := 8
-	expectedJ := 2
-	resultI, resultJ := findDivisible(input)
-	testDivisibleResult(input, expectedI, expectedJ, resultI, resultJ, t)
-
-	input = []int{9, 4, 7, 3}
-	expectedI = 9
-	expectedJ = 3
-	resultI, resultJ = findDivisible(input)
-	testDivisibleResult(input, expectedI, expectedJ, resultI, resultJ, t)
-
-	input = []int{3, 8, 6, 5}
-	expectedI = 6
-	expectedJ = 3
-	resultI, resultJ = findDivisible(input)
-	testDivisibleResult(input, expectedI, expectedJ, resultI, resultJ, t)
-}
-
-func testDivisibleResult(input []int, expectedI int, expectedJ int, resultI int, resultJ int, t *testing.T) {
-	if expectedI != resultI {
-		t.Error(input, "-> expected i of", expectedI, "but got", resultI)
+	test := []struct {
+		input     []int
+		expectedI int
+		expectedJ int
+	}{
+		{[]int{5, 9, 2, 8}, 8, 2},
+		{[]int{9, 4, 7, 3}, 9, 3},
+		{[]int{3, 8, 6, 5}, 6, 3},
 	}
-	if expectedJ != resultJ {
-		t.Error(input, "-> expected j of", expectedJ, "but got", resultJ)
+
+	for _, test := range test {
+		resultI, resultJ := findDivisible(test.input)
+
+		if test.expectedI != resultI {
+			t.Error(test.input, "-> expected i of", test.expectedI, "but got", resultI)
+		}
+		if test.expectedJ != resultJ {
+			t.Error(test.input, "-> expected j of", test.expectedJ, "but got", resultJ)
+		}
 	}
 }
