1c3d24e8a0633ea3dbd05dff650b0e6d2eeba48a
jasonlotz <jasonlotz@gmail.com>
2024-03-24 09:41:21 -0400

    feat: complete 2016 day 6 in Go

---
+++ go/2016/06/solution.go
@@ -0,0 +1,77 @@
+package main
+
+import (
+	"fmt"
+	"math"
+
+	"github.com/jasonlotz/advent-of-code/go/utils"
+)
+
+var inputFile = "../../../input-files/2016/06/input.txt"
+var testInputFile = "../../../input-files/2016/06/input-sample.txt"
+var isTestMode = false
+
+type LetterPositions []LetterMap
+type LetterMap map[string]int
+
+func getInput() []string {
+	file := inputFile
+
+	if isTestMode {
+		file = testInputFile
+	}
+
+	return utils.ProcessStringLinesFile(file)
+}
+
+func main() {
+	input := getInput()
+
+	letterPositions := countLetters(input)
+
+	messageMostCommon, messageLeastCommon := findMessage(letterPositions)
+
+	fmt.Println("Part 1:", messageMostCommon)
+	fmt.Println("Part 2:", messageLeastCommon)
+}
+
+func countLetters(input []string) LetterPositions {
+	letterPositions := make(LetterPositions, len(input[0]))
+
+	for i := range letterPositions {
+		letterCount := make(LetterMap)
+		letterPositions[i] = letterCount
+	}
+
+	for _, line := range input {
+		for i, letter := range line {
+			letterPositions[i][string(letter)]++
+		}
+	}
+
+	return letterPositions
+}
+
+func findMessage(letterPositions LetterPositions) (messageMostCommon string, messageLeastCommon string) {
+	for _, letterCount := range letterPositions {
+		maxLetter, minLetter := "", ""
+		maxCount := 0
+		minCount := math.MaxInt
+
+		for letter, count := range letterCount {
+			if count > maxCount {
+				maxLetter = letter
+				maxCount = count
+			}
+
+			if count < minCount {
+				minLetter = letter
+				minCount = count
+			}
+		}
+		messageMostCommon += maxLetter
+		messageLeastCommon += minLetter
+	}
+
+	return messageMostCommon, messageLeastCommon
+}
