1115e4ba983b1c2748427a4428e85b3213bcf1b3
jasonlotz <jasonlotz@gmail.com>
2024-03-26 21:03:46 -0400

    feat: complete 2020 day 6 in Go

---
+++ go/2020/06/solution.go
@@ -0,0 +1,82 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+
+	"github.com/jasonlotz/advent-of-code/go/utils"
+)
+
+var inputFile = "../../../input-files/2020/06/input.txt"
+var testInputFile = "../../../input-files/2020/06/input-sample.txt"
+var isTestMode = false
+
+func getInput() [][]string {
+	file := inputFile
+
+	if isTestMode {
+		file = testInputFile
+	}
+
+	input := strings.TrimSpace(utils.ProcessFullFile(file))
+
+	splitInput := strings.Split(input, "\n\n")
+
+	var result [][]string
+
+	for _, line := range splitInput {
+		result = append(result, strings.Split(line, "\n"))
+	}
+
+	return result
+}
+
+func main() {
+	part1()
+	part2()
+}
+
+func part1() {
+	input := getInput()
+	result := 0
+
+	for _, group := range input {
+		var answers = make(map[rune]bool)
+
+		for _, person := range group {
+			for _, answer := range person {
+				answers[answer] = true
+			}
+		}
+
+		for range answers {
+			result++
+		}
+	}
+
+	fmt.Println("Part 1: ", result)
+}
+
+func part2() {
+	input := getInput()
+
+	results := 0
+
+	for _, group := range input {
+		var answers = make(map[rune]int)
+
+		for _, person := range group {
+			for _, answer := range person {
+				answers[answer]++
+			}
+		}
+
+		for _, count := range answers {
+			if count == len(group) {
+				results++
+			}
+		}
+	}
+
+	fmt.Println("Part 2: ", results)
+}
