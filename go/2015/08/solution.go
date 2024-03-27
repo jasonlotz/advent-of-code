10b3623488c2429d5e38372e8296494cbb835451
jasonlotz <jasonlotz@gmail.com>
2024-03-27 12:07:10 -0400

    feat: complete 2015 day 8 in Go

---
+++ go/2015/08/solution.go
@@ -0,0 +1,96 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+
+	"github.com/jasonlotz/advent-of-code/go/utils"
+)
+
+var inputFile = "../../../input-files/2015/08/input.txt"
+var testInputFile = "../../../input-files/2015/08/input-sample.txt"
+var isTestMode = false
+
+func getInput() []string {
+	file := inputFile
+
+	if isTestMode {
+		file = testInputFile
+	}
+
+	return utils.ProcessStringLinesFile(file)
+}
+
+func main() {
+	part1()
+	part2()
+}
+
+func part1() {
+	input := getInput()
+	difference := 0
+
+	for _, line := range input {
+		processedLine := decode(line)
+
+		difference += len(line) - len(processedLine)
+	}
+
+	fmt.Println("Part 1: ", difference)
+}
+
+func part2() {
+	input := getInput()
+	difference := 0
+
+	for _, line := range input {
+		encodedLine := encode(line)
+
+		difference += len(encodedLine) - len(line)
+	}
+
+	fmt.Println("Part 2: ", difference)
+}
+
+func decode(line string) []rune {
+	var out []rune
+
+	// Remove the surrounding quotes
+	line = line[1 : len(line)-1]
+
+	for i := 0; i < len(line); i++ {
+		if line[i] == '\\' {
+			if line[i+1] == 'x' {
+				escInt, err := strconv.ParseInt(line[i+2:i+4], 16, 0)
+				if err != nil {
+					panic(err)
+				}
+				out = append(out, rune(escInt))
+				i += 3
+			} else {
+				out = append(out, rune(line[i+1]))
+				i++
+			}
+		} else {
+			out = append(out, rune(line[i]))
+		}
+
+	}
+
+	return out
+}
+
+func encode(line string) string {
+	encoded := "\""
+
+	for _, char := range line {
+		if char == '"' || char == '\\' {
+			encoded += "\\"
+		}
+		encoded += string(char)
+	}
+
+	encoded += "\""
+
+	return encoded
+}
