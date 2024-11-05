bfa5ec9e6c933b20893afa8ce33936dc62c145de
jasonlotz <jasonlotz@gmail.com>
2024-11-05 14:21:26 -0500

    feat: complete 2018 day 1 in Java

--- java/app/src/main/java/aoc/y2018/d01/Solution.java
+++ java/app/src/main/java/aoc/y2018/d01/Solution.java
@@ -1,7 +1,65 @@
 package aoc.y2018.d01;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 public class Solution {
+  private static final String INPUT_FILE = "../../../../../../../../input-files/2018/01/input.txt";
+
+  private List<String> readFile() {
+    try {
+      return Files.readAllLines(Paths.get(INPUT_FILE));
+    } catch (IOException e) {
+      throw new RuntimeException("Failed to read file");
+    }
+  }
+
+  public int sum(List<String> lines) {
+    return lines.stream().mapToInt(Integer::parseInt).sum();
+  }
+
+  public int findDuplicateFreq(List<String> lines) {
+    int totalFreq = 0;
+    int seenTwice = 0;
+    Set<Integer> seenFreqs = new HashSet<>();
+
+    // Loop through the list of frequencies until we find a duplicate
+    // frequency even if it means looping through the list multiple times
+    while (true) {
+      for (String line : lines) {
+        int freq = Integer.parseInt(line);
+
+        totalFreq += freq;
+
+        if (seenFreqs.contains(totalFreq)) {
+          seenTwice = totalFreq;
+          break;
+        }
+
+        seenFreqs.add(totalFreq);
+      }
+
+      if (seenTwice != 0) {
+        break;
+      }
+    }
+
+    return seenTwice;
+  }
+
   public static void main(String[] args) {
-    System.out.println("Hello World!");
+    Solution solution = new Solution();
+
+    List<String> lines = solution.readFile();
+
+    int totalFreq = solution.sum(lines);
+    int seenTwice = solution.findDuplicateFreq(lines);
+
+    System.out.println("Part 1: " + totalFreq);
+    System.out.println("Part 2: " + seenTwice);
   }
 }
