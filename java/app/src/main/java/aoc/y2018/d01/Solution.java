692f5dffa2cc85a8c3547ce567d34c9d3108679c
jasonlotz <jasonlotz@gmail.com>
2024-11-05 16:54:19 -0500

    refactor: minor updates to file pathing to input files

--- java/app/src/main/java/aoc/y2018/d01/Solution.java
+++ java/app/src/main/java/aoc/y2018/d01/Solution.java
@@ -2,17 +2,20 @@ package aoc.y2018.d01;
 
 import java.io.IOException;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
 public class Solution {
-  private static final String INPUT_FILE = "../../../../../../../../input-files/2018/01/input.txt";
+  // Run from the root of the root of the Gradle project
+  private static Path basePath = Paths.get(System.getProperty("user.dir"));
+  private static final Path INPUT_FILE = basePath.resolve("../../input-files/2018/01/input.txt");
 
   private List<String> readFile() {
     try {
-      return Files.readAllLines(Paths.get(INPUT_FILE));
+      return Files.readAllLines(INPUT_FILE);
     } catch (IOException e) {
       throw new RuntimeException("Failed to read file");
     }
