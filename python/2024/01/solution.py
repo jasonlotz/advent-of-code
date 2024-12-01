ae29f716e2f7d573c7dee3d61639fa167ad98cfc
jasonlotz <jasonlotz@gmail.com>
2024-12-01 10:13:40 -0500

    refactor: more pythonic implementation of 2024 day 1

--- python/2024/01/solution.py
+++ python/2024/01/solution.py
@@ -1,5 +1,6 @@
 import argparse
-from typing import Dict, List, Tuple
+from collections import Counter
+from typing import List, Tuple
 
 INPUT_PATH = "input-files/2024/01/input.txt"
 SAMPLE_INPUT_PATH = "input-files/2024/01/input-sample.txt"
@@ -29,9 +30,7 @@ def sum_total_distance():
     list1.sort()
     list2.sort()
 
-    total_distance = 0
-    for i in range(len(list1)):
-        total_distance += abs(list1[i] - list2[i])
+    total_distance = sum(abs(a - b) for a, b in zip(list1, list2))
 
     print(f"Total distance: {total_distance}")
 
@@ -40,20 +39,12 @@ def calc_similarity_score():
     input = read_input()
     list1, list2 = parse_input(input)
 
-    # Create a dict of the first list elements
-    list1_dict: Dict[int, int] = {
-        value: 0 for _, value in enumerate(list1)}
-
-    # Loop over the second list and count the occurrences of each element
-    for _, value in enumerate(list2):
-        if value in list1_dict:
-            list1_dict[value] += 1
+    counter = Counter()
+    counter.update(list2)
 
     # The similarity score is the sum of the product of the value
     # and the count of the value in the first list
-    similarity_score = 0
-    for _, value in enumerate(list1):
-        similarity_score += value * list1_dict[value]
+    similarity_score = sum(value * counter[value] for value in list1)
 
     print(f"Similarity score: {similarity_score}")
 
