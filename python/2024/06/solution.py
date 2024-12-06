d6051f3be2bb527ac22a47d06db64cc9ac237595
jasonlotz <jasonlotz@gmail.com>
2024-12-06 16:56:37 -0500

    feat: complete 2024 day 6 in Python

--- python/2024/06/solution.py
+++ python/2024/06/solution.py
@@ -46,8 +46,8 @@ def find_starting_point(input: List[str]) -> Tuple[int, int]:
     raise ValueError("No starting point found")
 
 
-def follow_path(input: List[str],
-                starting_point: Tuple[int, int]) -> Set[Tuple[int, int]]:
+def count_visits(grid: List[str],
+                 starting_point: Tuple[int, int]) -> Set[Tuple[int, int]]:
     direction = (0, -1)  # Start facing up
 
     visited: Set[Tuple[int, int]] = set()
@@ -64,12 +64,12 @@ def follow_path(input: List[str],
 
         # If we exit the grid, stop
         if (new_x < 0 or new_y < 0
-                or new_y >= len(input) or new_x >= len(input[new_y])):
+                or new_y >= len(grid) or new_x >= len(grid[new_y])):
             break
 
         # If we hit an obstacle, turn right
         # Otherwise, mark the location as visited and continue
-        if input[new_y][new_x] == "#":
+        if grid[new_y][new_x] == "#":
             direction = turn_right(direction)
         else:
             visited.add((new_x, new_y))
@@ -78,18 +78,88 @@ def follow_path(input: List[str],
     return visited
 
 
+# Use a fast and slow pointer to detect cycles
+def detect_cycle(grid: List[str],
+                 starting_point: Tuple[int, int]) -> bool:
+
+    slow_direction = (0, -1)
+    fast_direction = (0, -1)
+
+    slow_x, slow_y = starting_point
+    fast_x, fast_y = starting_point
+
+    while True:
+        # Move the slow pointer
+        new_slow_x = slow_x + slow_direction[0]
+        new_slow_y = slow_y + slow_direction[1]
+
+        # If we exit the grid, stop
+        if (new_slow_x < 0 or new_slow_y < 0
+                or new_slow_y >= len(grid)
+                or new_slow_x >= len(grid[new_slow_y])):
+            return False
+
+        # If we hit an obstacle, turn right
+        if (grid[new_slow_y][new_slow_x] == "#" or
+                grid[new_slow_y][new_slow_x] == "O"):
+            slow_direction = turn_right(slow_direction)
+        else:
+            slow_x, slow_y = new_slow_x, new_slow_y
+
+        # Move the fast pointer twice
+        for _ in range(2):
+            new_fast_x = fast_x + fast_direction[0]
+            new_fast_y = fast_y + fast_direction[1]
+
+            # If we exit the grid, stop
+            if (new_fast_x < 0 or new_fast_y < 0
+                    or new_fast_y >= len(grid)
+                    or new_fast_x >= len(grid[new_fast_y])):
+                return False
+
+            # If we hit an obstacle, turn right
+            if (grid[new_fast_y][new_fast_x] == "#" or
+                    grid[new_fast_y][new_fast_x] == "O"):
+                fast_direction = turn_right(fast_direction)
+            else:
+                fast_x, fast_y = new_fast_x, new_fast_y
+
+        # If the two pointers meet with the same direction, we have a cycle
+        if ((slow_x, slow_y) == (fast_x, fast_y) and
+                slow_direction == fast_direction):
+            return True
+
+
 def part1():
     input = parse_input(read_input())
 
     starting_point = find_starting_point(input)
 
-    visited = follow_path(input, starting_point)
+    visited = count_visits(input, starting_point)
 
     print(f"Visited {len(visited)} locations (part 1)")
 
 
 def part2():
-    pass
+    input = parse_input(read_input())
+
+    starting_point = find_starting_point(input)
+
+    visited = count_visits(input, starting_point)
+
+    detected_cycles = 0
+
+    for visited_x, visited_y in visited:
+        copy = [line[:] for line in input]
+
+        # Add an obstacle to the visited location
+        copy[visited_y] = copy[visited_y][:visited_x] + \
+            "O" + copy[visited_y][visited_x + 1:]
+
+        if detect_cycle(copy, starting_point):
+            detected_cycles += 1
+
+    print(f"Detected {detected_cycles} cycles (part 2)")
 
 
 def main():
